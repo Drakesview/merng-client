0e4b64fcab88627ac88481037cc98826a2878689
Adam Shenton <mini_shenton@hotmail.co.uk>
2021-05-06 10:38:35 +0100

    init

---
+++ src/ApolloProvider.js
@@ -0,0 +1,40 @@
+import React from "react";
+import App from "./App";
+import {
+  ApolloClient,
+  HttpLink,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const ApolloServer = () => {
+  const httpLink = createHttpLink({
+    uri: "http://localhost:5000",
+  });
+
+  const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken')
+    return{
+      headers: {
+        Authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  })
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+
+
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+export default ApolloServer;
